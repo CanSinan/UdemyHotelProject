6e36697c7fcc1b5c7116ecefc7dfd6b69fc87341
Sinan Can <Sinan51can@gmail.com>
2023-12-28 21:05:57 +0300

    Bölüm 7 = Identity Kütüphanesi - Register ve Login

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -1,20 +1,21 @@
 ﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelProject.DataAccessLayer.Concrete
 {
-	public class Context : DbContext
-	{
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			optionsBuilder.UseSqlServer("server=DESKTOP-LTMI9TN\\SQLEXPRESS;initial catalog=ApiDb;" +
-				"Persist Security Info=False;User ID=sa;Password=Password1;MultipleActiveResultSets=True;" +
-				"TrustServerCertificate=True;Connection Timeout=30;"); 
-		}
-		public DbSet<Room> Rooms { get; set; }
-		public DbSet<Service> Services{ get; set; }
-		public DbSet<Staff> Staffs{ get; set; }
-		public DbSet<Subscribe> Subscribes{ get; set; }
-		public DbSet<Testimonial> Testimonials { get; set; }
-	}
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LTMI9TN\\SQLEXPRESS;initial catalog=ApiDb;" +
+                "Persist Security Info=False;User ID=sa;Password=Password1;MultipleActiveResultSets=True;" +
+                "TrustServerCertificate=True;Connection Timeout=30;");
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
 }
---
+++ FrontEnd/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public RegisterController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateNewUserDto createNewUserDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var userMap = _mapper.Map<AppUser>(createNewUserDto);
+            var result = await _userManager.CreateAsync(userMap, createNewUserDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index","Login");
+            }
+
+            return View();
+        }
+    }
+}
